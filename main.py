6131edcd172cbe52104b4348efff5f86fb08e29a
YeongRoYun <58219267+YeongRoYun@users.noreply.github.com>
2023-11-28 16:39:47 +0900

    [feat] login view (#1)

---
+++ main.py
@@ -0,0 +1,20 @@
+from nicegui import app, ui
+
+from mylib.common.middleware.auth import AuthMiddleware
+
+
+@ui.page("/")
+def main_page() -> None:
+    with ui.column().classes("absolute-center items-center"):
+        ui.label(f'Hello {app.storage.user["username"]}!').classes("text-2xl")
+        ui.button(on_click=lambda: (app.storage.user.clear(), ui.open("/login")), icon="logout").props("outline round")
+
+
+if __name__ in {"__main__", "__mp_main__"}:
+    # import pages
+    from mylib.domain.login.view import login
+
+    views = [login]
+
+    app.add_middleware(AuthMiddleware)
+    ui.run(storage_secret="happy development!!")
